91040f5eb724032f54be431c0fc86b8d39745d96
luizDiasDev <luiz.dias602120@gmail.com>
2026-09-19 19:48:33 -0300

    feat: create audit model

--- validador_presenca/apps/audit/models.py
+++ validador_presenca/apps/audit/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class AuditLog(models.Model):
+    autor_id = models.IntegerField()
+    ocorrido_em = models.DateField(auto_now_add=True)
+    origem = models.CharField(max_length=50)
+    acao = models.CharField(max_length=50)
+    tabela = models.CharField(max_length=50)
+    linha_tabela_id = models.BigIntegerField()
+
+class Meta:
+    db_table  = "log_auditoria"
+    verbose_name = "Log de Auditoria"
+    verbose_name_plural = "Logs de Auditoria"
+
