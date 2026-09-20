cbd187d84339ac4a6c4a08ee8cc3bb18e0c59a9c
luizDiasDev <luiz.dias602120@gmail.com>
2026-09-19 23:11:17 -0300

    fix: correct audit config

--- validador_presenca/apps/audit/models.py
+++ validador_presenca/apps/audit/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 class AuditLog(models.Model):
     autor_id = models.IntegerField()
-    ocorrido_em = models.DateField(auto_now_add=True)
+    ocorrido_em = models.DateTimeField(auto_now_add=True)
     origem = models.CharField(max_length=50)
     acao = models.CharField(max_length=50)
     tabela = models.CharField(max_length=50)
     linha_tabela_id = models.BigIntegerField()
 
-class Meta:
-    db_table  = "log_auditoria"
-    verbose_name = "Log de Auditoria"
-    verbose_name_plural = "Logs de Auditoria"
+    class Meta:
+        db_table  = "log_auditoria"
+        verbose_name = "Log de Auditoria"
+        verbose_name_plural = "Logs de Auditoria"
 
--- validador_presenca/validador_presenca/settings.py
+++ validador_presenca/validador_presenca/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.checkin',
     'apps.validator',
+    'apps.audit',
 ]
 
 MIDDLEWARE = [
