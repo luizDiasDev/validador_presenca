35dd5cfe29d498250167e0f99030eb724b28015d
Fabriciors9 <fabricio.infom@gmail.com>
2026-09-20 15:23:57 -0300

    refactor: remove Machine in checkin and moved to institution

--- validador_presenca/apps/checkin/models.py
+++ validador_presenca/apps/checkin/models.py
@@ -2,26 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Machine(models.Model):
-    # Definindo Colunas
-    apelido = models.CharField(max_length=20)
-    codigo_pareamento = models.CharField(max_length=4, unique=True)
-    ip = models.GenericIPAddressField(null=True, blank=True)
-    visto_em = models.DateTimeField(null=True, blank=True)
-    data_criacao = models.DateTimeField(auto_now_add=True)
-    data_atualizacao = models.DateTimeField(auto_now=True)
-
-    # Nome que vai aparecer
-    class Meta:
-        db_table = "maquina"
-        verbose_name = "Máquina"
-        verbose_name_plural = "Máquinas"
-
-    # Como vai retornar
-    def __str__(self):
-        return f"Máquina {self.codigo_pareamento}"
-
 class ChamadaSession(models.Model):
+    # Definindo Colunas
     sequencia = models.IntegerField(default=1)
     abre_em = models.DateTimeField()
     fecha_em = models.DateTimeField()
@@ -29,11 +11,13 @@ class ChamadaSession(models.Model):
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_atualizacao = models.DateTimeField(auto_now=True)
 
+    # Nome que vai aparecer
     class Meta:
         db_table = "sessao_chamada"
         verbose_name = "Sessão de Chamada"
         verbose_name_plural = "Sessões de Chamada"
         ordering = ["-abre_em"]
 
+    # Como vai retornar
     def __str__(self):
         return f"Sessão Chamada #{self.pk}. Abre em: ({self.abre_em:%d/%m %H:%M}). Fecha em ({self.fecha_em:%d/%m %H:%M})"
