50ab237b4feba54021f50ec3e60dc6fe24b579e6
luizDiasDev <luiz.dias602120@gmail.com>
2026-09-24 23:38:21 -0300

    chore: add argon2 to settings

--- validador_presenca/validador_presenca/settings.py
+++ validador_presenca/validador_presenca/settings.py
@@ -121,6 +121,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
